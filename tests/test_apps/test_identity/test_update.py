8ff806204719e90ea1c627b1fac9e76bb279ca6e
Dmitry Kolyagin <dmitry.kolyagin@gmail.com>
2023-03-24 08:06:41 +0900

    Test valid update

--- tests/test_apps/test_identity/test_update.py
+++ tests/test_apps/test_identity/test_update.py
@@ -12,25 +12,18 @@ if TYPE_CHECKING:
 
 
 @pytest.mark.django_db
-def test_valid_registration(
-    client: Client,
-    user_email: str,
-    user_password: str,
+def test_valid_update(
+    admin_client: Client,
+    admin_user: User,
     user_profile_data: 'ProfileData',
     assert_user_profile: 'ProfileAssertion',
 ) -> None:
-    auth_data = {
-        'email': user_email,
-        'password1': user_password,
-        'password2': user_password,
-    }
-    response = client.post(
-        '/identity/registration',
-        data=user_profile_data | auth_data,
+    response = admin_client.post(
+        '/identity/update',
+        data=user_profile_data,
     )
     assert response.status_code == HTTPStatus.FOUND, (
         response.context['form'].errors
     )
-    user = User.objects.all().get(email=user_email)
-    assert user.check_password(user_password)
-    assert_user_profile(user, user_profile_data)
+    admin_user.refresh_from_db()
+    assert_user_profile(admin_user, user_profile_data)
