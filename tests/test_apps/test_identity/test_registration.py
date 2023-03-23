3eb2c9bdbf86090f06fd45eaea88a48b5bc61abd
Dmitry Kolyagin <dmitry.kolyagin@gmail.com>
2023-03-23 17:18:14 +0900

    Test successful registration

---
+++ tests/test_apps/test_identity/test_registration.py
@@ -0,0 +1,32 @@
+from http import HTTPStatus
+from typing import Any, Callable, Mapping
+
+from django.test import Client
+import pytest
+
+from server.apps.identity.models import User
+
+
+@pytest.mark.django_db
+def test_success(
+    client: Client,
+    user_email: str,
+    user_password: str,
+    user_profile_data: Mapping[str, Any],
+    assert_user_profile: Callable[[User, Mapping[str, Any]], None],
+) -> None:
+    credentials = {
+        'email': user_email,
+        'password1': user_password,
+        'password2': user_password,
+    }
+    response = client.post(
+        '/identity/registration',
+        data=credentials | user_profile_data,
+    )
+    assert response.status_code == HTTPStatus.FOUND, (
+        response.context['form'].errors
+    )
+    user = User.objects.all().get(email=user_email)
+    assert user.check_password(user_password)
+    assert_user_profile(user, user_profile_data)
