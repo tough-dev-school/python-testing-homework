6f624bfb38f4ab854e5f0080e1d666165ac406c1
mvasilchenko <michael@vasilchenko.dev>
2023-03-25 00:45:34 +0200

    style(flake8): prettify

--- tests/plugins/identity/user.py
+++ tests/plugins/identity/user.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, final, Callable
+from typing import Callable, TypedDict
 
 import faker
 import pytest
@@ -11,12 +11,16 @@ fake = faker.Faker()
 
 
 class UserCredentials(TypedDict):
+    """User credentials for registration."""
+
     email: str
     password1: str
     password2: str
 
 
 class UserData(TypedDict):
+    """User data for registration."""
+
     first_name: str
     last_name: str
     date_of_birth: str
@@ -25,36 +29,43 @@ class UserData(TypedDict):
     phone: str
 
 
-@pytest.fixture
+@pytest.fixture()
 def user_credentials() -> UserCredentials:
+    """Generate user credentials for registration."""
     password = fake.password()
-    data = {
-        "email": fake.email(),
-        "password1": password,
-        "password2": password,
+    user_credentials = {
+        'email': fake.email(),
+        'password1': password,
+        'password2': password,
     }
-    return UserCredentials(**data)
+    return UserCredentials(
+        **user_credentials,
+    )
 
 
-@pytest.fixture
+@pytest.fixture()
 def user_data() -> UserData:
+    """Generate user data for registration."""
+    user_data = {
+        'first_name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'date_of_birth': fake.date_of_birth(),
+        'address': fake.address(),
+        'job_title': fake.job(),
+        'phone': fake.phone_number(),
+    }
     return UserData(
-        **{
-            "first_name": fake.first_name(),
-            "last_name": fake.last_name(),
-            "date_of_birth": fake.date_of_birth(),
-            "address": fake.address(),
-            "job_title": fake.job(),
-            "phone": fake.phone_number(),
-        }
+        **user_data,
     )
 
 
 UserAssertion: TypeAlias = Callable[[UserCredentials, UserData], None]
 
 
-@pytest.fixture
+@pytest.fixture()
 def assert_user_was_created() -> UserAssertion:
+    """Assert that user was created with correct data."""
+
     def factory(user_credentials: UserCredentials, user_data: UserData) -> None:
         user = User.objects.get(email=user_credentials['email'])
         assert user.id
@@ -70,6 +81,7 @@ def assert_user_was_created() -> UserAssertion:
     return factory
 
 
-@pytest.fixture
+@pytest.fixture()
 def user():
+    """Create a user instance."""
     return mixer.blend(User)
--- tests/test_apps/test_identity/test_signup.py
+++ tests/test_apps/test_identity/test_signup.py
@@ -7,51 +7,60 @@ from django.urls import reverse
 from server.apps.identity.models import User
 
 
-@pytest.mark.django_db
+@pytest.mark.django_db()
 def test_signup(
     client: Client,
-    user_credentials: 'UserCredentials',
-    user_data: 'UserData',
-    assert_user_was_created: 'UserAssertion'
+    user_credentials,
+    user_data,
+    assert_user_was_created,
 ):
+    """Test user registration."""
     response = client.post(
-        reverse("identity:registration"),
+        reverse('identity:registration'),
         data={**user_credentials, **user_data},
     )
 
     assert response.status_code == HTTPStatus.FOUND
-    assert not hasattr(response.context, 'form')
     assert_user_was_created(user_credentials, user_data)
 
 
-@pytest.mark.django_db
+@pytest.mark.django_db()
 @pytest.mark.parametrize('field', User.REQUIRED_FIELDS + [User.USERNAME_FIELD])
 def test_missing_required_fields(
     client: Client,
-    user_credentials: 'UserCredentials',
-    user_data: 'UserData',
-    assert_user_was_created: 'UserAssertion',
+    user_credentials,
+    user_data,
+    assert_user_was_created,
     field: str,
 ):
+    """Test user registration with missing required fields."""
     response = client.post(
-        reverse("identity:registration"),
-        data={**user_credentials, **user_data, **{field: ''}}
+        reverse('identity:registration'),
+        data={
+            **user_credentials,
+            **user_data,
+            **{field: ''},
+        },
     )
 
     assert response.status_code == HTTPStatus.OK
 
 
-@pytest.mark.django_db
+@pytest.mark.django_db()
 def test_user_already_exists(
     client: Client,
     user: User,
-    user_credentials: 'UserCredentials',
-    user_data: 'UserData',
+    user_credentials,
+    user_data,
 ):
-
+    """Test user registration with already existing user."""
     response = client.post(
-        reverse("identity:registration"),
-        data={**user_credentials, **user_data, **{User.USERNAME_FIELD: user.email}},
+        reverse('identity:registration'),
+        data={
+            **user_credentials,
+            **user_data,
+            **{User.USERNAME_FIELD: user.email},
+        },
     )
-    assert len(response.context['form'].errors) >= 1
+    assert response.context['form'].errors
     assert response.status_code == HTTPStatus.OK
