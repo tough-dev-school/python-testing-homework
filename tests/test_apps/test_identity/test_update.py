43245ad1b14ec634b9aede57ba89fd977d913d6f
mvasilchenko <michael@vasilchenko.dev>
2023-03-25 15:19:39 +0200

    test(identity): add test for invalid update with missing fields

--- tests/test_apps/test_identity/test_update.py
+++ tests/test_apps/test_identity/test_update.py
@@ -4,6 +4,8 @@ import pytest
 from django.test import Client
 from django.urls import reverse
 
+from server.apps.identity.models import User
+
 
 @pytest.mark.django_db()
 def test_valid_update(
@@ -21,3 +23,25 @@ def test_valid_update(
 
     assert response.status_code == HTTPStatus.FOUND
     assert_user_update(user, user_data)
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize('field', User.REQUIRED_FIELDS)
+def test_invalid_update(
+    client: Client,
+    user,
+    user_data,
+    field: str,
+):
+    """Test user update with invalid data."""
+    client.force_login(user)
+    response = client.post(
+        reverse('identity:user_update'),
+        data={
+            **user_data,
+            **{field: ''},
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.context['form'].errors
