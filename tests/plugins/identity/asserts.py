155e33cb48d709b5194e88b9eea3e14f1bd8dd85
vgsoldatkin <vasiliy.soldatkin@yandex.ru>
2023-03-25 20:33:57 +0300

    fix asserts

--- tests/plugins/identity/asserts.py
+++ tests/plugins/identity/asserts.py
@@ -16,6 +16,10 @@ def assert_correct_user():
         user = User.objects.get(email=email)
         assert user.email == expected['email']
         assert user.job_title == expected['job_title']
+        assert user.address == expected['address']
+        assert user.last_name == expected['last_name']
+        assert user.first_name == expected['first_name']
+        assert user.date_of_birth == expected['date_of_birth']
 
     return factory
 
