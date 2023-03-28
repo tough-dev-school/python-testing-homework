ed74b26673e7e930661cfd3c8692db92d53c8eb5
Alexander Burchenko <aabur@mail.ru>
2023-03-29 00:15:34 +0200

    add test for user update view

--- tests/test_apps/test_identity/test_views.py
+++ tests/test_apps/test_identity/test_views.py
@@ -57,3 +57,33 @@ def test_registration_view_post(
     assert response.status_code == HTTPStatus.FOUND
     assert response.get('Location', '') == reverse('identity:login')
     assert User.objects.filter(email=user_data['email']).exists()
+
+
+def test_user_update_view(client: Client, user_data: dict[str, str]) -> None:
+    """Test accessing and updating the UserUpdateView."""
+    user = User.objects.create_user(**user_data)
+    client.force_login(user)
+
+    url = reverse('identity:user_update')
+    response = client.get(url)
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'csrfmiddlewaretoken' in response.content.decode()
+    assert response.context['user'] == user
+
+    # TODO: change for a fixture
+    updated_data = {
+        'first_name': 'UpdatedFirstName',
+        'last_name': 'UpdatedLastName',
+        'address': '123 Main St, Anytown',
+        'job_title': 'Software Engineer',
+        'phone': '+1 123-456-7890',
+    }
+    response = client.post(url, updated_data)
+
+    assert response.status_code == HTTPStatus.FOUND
+    assert response.get('Location', '') == reverse('identity:user_update')
+
+    user.refresh_from_db()
+    assert user.first_name == updated_data['first_name']
+    assert user.last_name == updated_data['last_name']
