36f6001d9166726775da077c10cb3c8c0f017064
Alexander Burchenko <aabur@mail.ru>
2023-03-29 13:46:18 +0200

    add pictures views tests (not realised)

---
+++ tests/test_apps/test_pictures/test_pictures_views.py
@@ -0,0 +1,19 @@
+"""Test the pictures views."""
+import pytest
+
+pytestmark = pytest.mark.django_db
+
+
+def test_index_view():
+    """Test the index view."""
+    # TODO: implement this test
+
+
+def test_dashboard_view():
+    """Test the dashboard view."""
+    # TODO: implement this test
+
+
+def test_favourite_pictures_view():
+    """Test the favourite pictures view."""
+    # TODO: implement this test
