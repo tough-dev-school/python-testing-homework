a911f09fe77993b0fafeb78b308cdf21dbc1ede1
Ivan Kavaliou <info@ikovalev.com>
2023-04-02 02:33:13 +0300

    Get frozen user id for all requests

--- data/delay.middleware.js
+++ data/delay.middleware.js
@@ -1,5 +1,12 @@
 module.exports = (req, res, next) => {
   setTimeout(() => {
+
+    if (req.originalUrl.includes('users')) {
+      res.json({
+        id: 584
+      })
+    }
+
     next()
   }, 2000);
 }
