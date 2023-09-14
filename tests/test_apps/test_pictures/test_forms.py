eabefb036afc02fbd33dd8567d281529fdb4c109
unknown <vanobel159@gmail.com>
2023-09-14 22:41:10 +0300

    Fix forms tests

--- tests/test_apps/test_pictures/test_forms.py
+++ tests/test_apps/test_pictures/test_forms.py
@@ -17,16 +17,22 @@ def picture_raw_data(picture):
     yield {"foreign_id": picture.foreign_id, "url": picture.url}
 
 
-def test_favourites_form_correct_with_user(user, picture_raw_data, picture):
+@pytest.mark.parametrize(
+    "save, expected", [
+        (True, 2),
+        (False, 1)
+    ]
+)
+def test_favourites_form_correct_with_user(user, picture_raw_data, save, expected):
     form = FavouritesForm(data=picture_raw_data, user=user)
-    form.save()
-    assert FavouritePicture.objects.count() == 2
+    form.save(save)
+    assert form.is_valid()
+    assert FavouritePicture.objects.count() == expected
 
 
-def test_favourites_form_fail_without_user(picture_raw_data, picture):
+def test_favourites_form_fail_without_user(picture_raw_data):
     with pytest.raises(KeyError):
-        form = FavouritesForm(data=picture_raw_data)
-        form.save()
+        FavouritesForm(data=picture_raw_data)
 
     """expected count == 1 because picture fixture create one record, but form doesn't create"""
     assert FavouritePicture.objects.count() == 1
