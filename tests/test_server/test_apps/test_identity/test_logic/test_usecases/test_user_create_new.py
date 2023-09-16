f8fcbc47ff01933d700a3c6d86e82091727db923
Roman Smirnov <rosmirnov@at-consulting.ru>
2023-09-16 14:22:56 +0300

    feat: add tests for user_create_new.py

---
+++ tests/test_server/test_apps/test_identity/test_logic/test_usecases/test_user_create_new.py
@@ -0,0 +1,41 @@
+import pytest
+
+from server.apps.identity.intrastructure.services.placeholder import UserResponse
+from server.apps.identity.logic.usecases.user_create_new import UserCreateNew
+from server.apps.identity.models import User
+from server.common.django.types import Settings
+
+
+class TestUserCreateNew:
+    @pytest.mark.django_db
+    def test_success_create_new_user(
+        self,
+        user: User,
+        settings: Settings,
+        assert_correct_user,
+        reg_data,
+        expected_user_data,
+    ) -> None:
+        not_lead_user = User.objects.get(email=reg_data["email"])
+        assert not_lead_user.lead_id is None
+        assert_correct_user(reg_data["email"], expected_user_data)
+
+        UserCreateNew(settings=settings)(user=user)
+        lead_user = User.objects.get(email=reg_data["email"])
+        expected_lead_id = 11
+        assert lead_user.lead_id == expected_lead_id
+
+    @pytest.mark.django_db
+    def test_success__update_user_ids(
+        self,
+        user: User,
+        settings: Settings,
+        assert_correct_user,
+        reg_data,
+        expected_user_data,
+    ) -> None:
+        assert_correct_user(reg_data["email"], expected_user_data)
+        response = UserResponse(id=100)
+        UserCreateNew(settings=settings)._update_user_ids(user=user, response=response)
+        actual_user = User.objects.get(email=reg_data["email"])
+        assert actual_user.lead_id == response.id
