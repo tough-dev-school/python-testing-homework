cc388aa75912a914f804130a3289958347160525
Ratotoskr <ratotoskr@deeplay.io>
2023-09-23 14:33:41 +0400

    Create user identity plugin

--- tests/test_apps/test_identity/__init__.py
+++
@@ -1,4 +0,0 @@
-"""better-bans-kedro
-"""
-
-__version__ = "0.1"
--- tests/test_apps/test_identity/test_registration.py
+++ tests/test_apps/test_identity/test_registration.py
@@ -4,11 +4,11 @@ import pytest
 from django.test import Client
 from django.urls import reverse
 
-from server.apps.identity.models import User
-from .conftest import RegistrationData, UserAssertion, LoginData
+pytestmark = pytest.mark.django_db
+
+from tests.plugins.identity.user import RegistrationData, UserDataExtractor, UserAssertion
 
 
-@pytest.mark.django_db()
 @pytest.mark.parametrize('page', ['/identity/login', '/identity/registration'])
 def test_identity_pages_unauthenticated(client: Client, page: str) -> None:
     """test accessibility of identity pages for unauthenticated users"""
@@ -18,7 +18,6 @@ def test_identity_pages_unauthenticated(client: Client, page: str) -> None:
     assert response.status_code == HTTPStatus.OK
 
 
-@pytest.mark.django_db()
 @pytest.mark.parametrize('page', ['/pictures/dashboard', '/pictures/favourites'])
 def test_pictures_pages_unauthenticated(client: Client, page: str) -> None:
     """test ensures that unauthenticated users are redirected to login page"""
@@ -29,10 +28,10 @@ def test_pictures_pages_unauthenticated(client: Client, page: str) -> None:
     assert response.url == '/identity/login?next=' + page
 
 
-@pytest.mark.django_db()
 def test_valid_registration(
         client: Client,
         registration_data: RegistrationData,
+        user_data: UserDataExtractor,
         assert_correct_user: UserAssertion,
 ) -> None:
     """Test that registration works with correct user data."""
@@ -40,22 +39,10 @@ def test_valid_registration(
         reverse('identity:registration'),
         data=registration_data,
     )
+
     assert response.status_code == HTTPStatus.FOUND
     assert response.get('Location') == reverse('identity:login')
-    assert_correct_user(registration_data)
-
-
-@pytest.mark.django_db()
-def test_user_login(
-        client: Client,
-        create_new_user: User,
-        login_data: LoginData,
-) -> None:
-    """Check user login."""
-    response = client.post(
-        reverse('identity:login'),
-        data=login_data,
+    assert_correct_user(
+        registration_data['email'],
+        user_data(registration_data),
     )
-
-    assert response.status_code == HTTPStatus.FOUND
-    assert response.get('Location') == reverse('pictures:dashboard')
